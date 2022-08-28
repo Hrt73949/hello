8b9adfca8a6caaa0feb470cdf20d55028cb2ccd9
Hrt73949 <hrt73949@cox.net>
2022-08-28 03:50:36 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Michael Hartley
+ *         hrt73949@cox.net
+ * Date: 2022/10/27
+ *
+ * A simple Print your Name program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Michael Hartley\n");
+
+  return 0;
+}
